027e3d282e9622ca7870ab3131ca50cba081958c
阮创 <rnchg@hotmail.com>
2025-12-13 22:57:18 +0800

    Update V 2.9.16.0

--- src/Apt.App/Usings.cs
+++ src/Apt.App/Usings.cs
@@ -1,12 +1,8 @@
 ﻿global using Microsoft.Extensions.DependencyInjection;
 global using Microsoft.Extensions.Hosting;
-global using System;
 global using System.Collections.ObjectModel;
 global using System.IO;
-global using System.Linq;
 global using System.Reflection;
-global using System.Threading;
-global using System.Threading.Tasks;
 global using System.Windows;
 global using System.Windows.Input;
 global using System.Windows.Threading;
